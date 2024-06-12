5e005bfc0feb4e76330d33d9663c6a5fb8c26b51
AbdoulrahmanGH <115339744+AbdoulrahmanGH@users.noreply.github.com>
2024-06-12 12:13:11 +0800

    Build Platform Selector and change logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+//import logo from "../assets/logo.webp";
+import logo2 from "../assets/GameLogo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="12px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo2} boxSize="100px" />
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
